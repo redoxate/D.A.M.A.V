6718890d86e05ae6f127b96cc7126799fd312ac1
Mohamed Reda Wichou <reda@oqtolab.com>
2020-02-12 15:33:00 +0100

    add ft

--- hc.py
+++ hc.py
@@ -2,39 +2,42 @@
 import RPi.GPIO as GPIO
 import time
 
-try:
-      GPIO.setmode(GPIO.BOARD)
+while True:
+      try:  
+            GPIO.setmode(GPIO.BOARD)
 
-      PIN_TRIGGER = 7
-      PIN_ECHO = 11
+            PIN_TRIGGER = 7
+            PIN_ECHO = 11
 
-      GPIO.setup(PIN_TRIGGER, GPIO.OUT)
-      GPIO.setup(PIN_ECHO, GPIO.IN)
+            GPIO.setup(PIN_TRIGGER, GPIO.OUT)
+            GPIO.setup(PIN_ECHO, GPIO.IN)
 
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+            GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-      print "Attent la stabilisation du sensor"
+            print "Attent la stabilisation du sensor"
 
-      time.sleep(2)
+            time.sleep(2)
 
-      print "Calcul la distance"
+            print "Calcul la distance"
 
-      GPIO.output(PIN_TRIGGER, GPIO.HIGH)
+            GPIO.output(PIN_TRIGGER, GPIO.HIGH)
 
-      time.sleep(0.00001)
+            time.sleep(0.00001)
 
-      GPIO.output(PIN_TRIGGER, GPIO.LOW)
+            GPIO.output(PIN_TRIGGER, GPIO.LOW)
 
-      while GPIO.input(PIN_ECHO)==0:
-            pulse_start_time = time.time()
-      while GPIO.input(PIN_ECHO)==1:
-            pulse_end_time = time.time()
+            while GPIO.input(PIN_ECHO)==0:
+                  pulse_start_time = time.time()
+            while GPIO.input(PIN_ECHO)==1:
+                  pulse_end_time = time.time()
 
-      pulse_duration = pulse_end_time - pulse_start_time
-      distance = round(pulse_duration * 17150, 2)
-      print "Distance:",distance,"cm"
+            pulse_duration = pulse_end_time - pulse_start_time
+            distance = round(pulse_duration * 17150, 2)
+            print "Distance:",distance,"cm"
 
-finally:
-      GPIO.cleanup()
+      finally:
+            GPIO.cleanup()
+            
+
 
 
